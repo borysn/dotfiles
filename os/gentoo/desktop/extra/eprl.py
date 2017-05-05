6447bf40de5e29f5bd84bf71949d94fc60a08a93
borysn <xborysn@gmail.com>
2017-05-05 23:15:20 +0000

    updates

---
+++ os/gentoo/desktop/extra/eprl.py
@@ -0,0 +1,157 @@
+#!/bin/env python3
+
+#
+# eprl.py
+# author: borysn | oxenfree
+# license: MIT
+#
+# edit portage resume list
+#
+
+import portage, sys, argparse
+
+# ANSI color codes
+# thank you, lliam-mcinroy
+# https://stackoverflow.com/a/22886382/2276284
+class bcolors:
+    HEADER  = '\033[95m'
+    OKBLUE  = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARN    = '\033[93m'
+    ERROR   = '\033[91m'
+    ENDC    = '\033[0m'
+
+    def disable(self):
+        self.HEADER  = ''
+        self.OKBLUE  = ''
+        self.OKGREEN = ''
+        self.WARN    = ''
+        self.ERROR   = ''
+        self.ENDC    = ''
+
+# status
+# status log msg prefixes (with colors)
+class status:
+    ERROR   = bcolors.ERROR   + 'ERROR'   + bcolors.ENDC  
+    WARN    = bcolors.WARN    + 'WARNING' + bcolors.ENDC
+    INFO    = bcolors.OKBLUE  + 'INFO'    + bcolors.ENDC
+    SUCCESS = bcolors.OKGREEN + 'SUCCESS' + bcolors.ENDC
+
+# parse command line options and arguments
+def parseArgs():
+    # get argument parser
+    parser = argparse.ArgumentParser()
+    # list portage resume items
+    parser.add_argument('-l', '--list', action='store_true', help='list portage resume items')
+    # remove portage resume item(s)
+    parser.add_argument('-r', '--remove', action='store', dest='itemNum', type=int, help='remove portage resume item') 
+    # version
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+
+    # parse arguments and return
+    return parser.parse_args()
+
+# canRemoveItem
+# validate if an item can be removed from the resume point
+#
+# @param itemNum    valid portage resume item for removal
+# @return           True if item can be removed, False otherwise
+def canRemoveItem(itemNum):
+    return True
+
+# argsAreValid
+# validate commandline options and arguments
+#
+# note: argparse will handle most of the args error checking
+#
+# @param args      
+# @return        True if args are valid, False otherwise
+def argsAreValid(args): 
+    # no options specified
+    if args.list == None and args.itemNum == None:
+        # no options specified, arparse didn't parse anything either
+        print(status.ERROR + ': what to do, what to do... try -h')
+        # args not valid
+        return False
+    # itemNum specified
+    elif args.itemNum != None:
+        # check if itemNum is available for removal
+        if canRemoveItem(args.itemNum) != True:
+            # error
+            print(status.ERROR + ': invalid item number "%s", cannot remove' % args.itemNum)
+            # itemNum not available for removal, arg not valid
+            return False
+    # all args valid, return True
+    return True
+
+# printResumeItems
+# output resume item list to console
+#
+# @params items    dictionary containing 'resume' & 'resume_backup' matrices
+def printResumeItems(allItems):
+    # print all items
+    for name,items in allItems.items():
+        # print collection name
+        print('\t[%s]' % name)
+        # check list size
+        if items == None or len(items) <= 0:
+            print(status.WARN + ': list is empty')
+        else:
+            for i in len(items):
+                print('\t [#%d]' % i + items[i])
+
+# listPortageResumeItems
+# list all ebuilds scheduled in resume & resume_backup
+def listPortageResumeItems():
+    try:
+        # get resume items
+        resume       = portage.mtimedb.get('resume', {}).get("mergelist")
+        resumeBackup = portage.mtimedb.get('resume_backup', {}).get("mergelist")
+        items = {'resume': resume, 'resume_backup': resumeBackup}
+        # print resume items
+        printResumeItems(items)
+        # listing portage resume items was successful
+        print(status.SUCCESS + ': [portage resume items list]')
+    except:
+        # listing portage resume items was successful
+        print(status.ERROR + ': [cannot list portage resume items]')
+        sys.exit(2)
+
+# removePortageResumeItem
+# remove a portage resume item
+#
+# @param itemNum    valid portage resume item to be removed
+def removePortageResumeItem(itemNum):
+    print(status.SUCCESS + ': [item "%s" removed from portage resume list]' % itemNum)
+
+# runScript
+# run script as a function of args
+#
+# @param args       args should be validated before passed in
+def runScript(args):
+    # list all portage emerge items
+    if args.list == True:
+        listPortageResumeItems()
+    # remove portage resume item(s)
+    if args.itemNum != None:
+        removePortageResumeItem(args.itemNum)
+
+# main
+# parse & validate args, run script
+def main():
+    # get args
+    args = parseArgs()
+
+    # validate args, exit if invalid
+    if argsAreValid(args) == False:
+        sys.exit(2)
+        
+    # execute script
+    runScript(args)
+
+    # exit succes
+    sys.exit(0)
+
+# exec main
+if __name__ == '__main__':
+    main()
